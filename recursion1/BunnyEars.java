5f5ceb7558a7cfbd23293d156c12495009a359e7
Joanna Zawislak <148885748+JoannaZawislak549@users.noreply.github.com>
2025-06-18 19:56:23 +0200

    Update BunnyEars.java

--- recursion1/BunnyEars.java
+++ recursion1/BunnyEars.java
@@ -1,3 +1,13 @@
+/* https://codingbat.com/prob/p183649
+
+We have a number of bunnies and each bunny has two big floppy ears. 
+We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+bunnyEars(0) → 0
+bunnyEars(1) → 2
+bunnyEars(2) → 4
+*/
+
 public class BunnyEars {
     public static int bunnyEars2(int bunnies) {
         if (bunnies == 0){
