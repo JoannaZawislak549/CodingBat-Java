4a8c6bebdd4c12de17ae38da42184234e2017ce9
Joanna Zawislak <148885748+JoannaZawislak549@users.noreply.github.com>
2025-06-18 19:58:50 +0200

    Update allStar.java

--- recursion1/allStar.java
+++ recursion1/allStar.java
@@ -1,3 +1,11 @@
+/* https://codingbat.com/prob/p183394
+
+Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+
+allStar("hello") → "h*e*l*l*o"
+allStar("abc") → "a*b*c"
+allStar("ab") → "a*b"
+*/
 public class allStar {
     public static String allStar(String str) {
 
