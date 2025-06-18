875c5146136048535c2cd85ee86c91f0771c68ab
Joanna Zawislak <148885748+JoannaZawislak549@users.noreply.github.com>
2025-06-18 19:57:16 +0200

    Update Count7.java

--- recursion1/Count7.java
+++ recursion1/Count7.java
@@ -1,3 +1,14 @@
+/* https://codingbat.com/prob/p101409
+
+Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. 
+(no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), 
+while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+count7(717) → 2
+count7(7) → 1
+count7(123) → 0
+*/
+
 public class Count7 {
     public static int count7(int n) {
         if (n < 7) {return 0;}
